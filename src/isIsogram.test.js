da2fde28cf9564fb57cd67714ee9f570df429f17
Albina <yakunina.albina@gmail.com>
2025-12-03 15:27:15 +0100

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -3,7 +3,17 @@
 describe('isIsogram', () => {
   const { isIsogram } = require('./isIsogram');
 
-  it(`should be declared`, () => {
-    expect(isIsogram).toBeInstanceOf(Function);
+  it(`should return true if 'word' === ''`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`should return false if 'word' includes repeating letters.`, () => {
+    expect(isIsogram('Oops')).toBe(false);
+    expect(isIsogram('look')).toBe(false);
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it(`should return false if 'word' includes repeating letters.`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
   });
 });
